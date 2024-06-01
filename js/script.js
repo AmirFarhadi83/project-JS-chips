6d970147b24af0a88c5a1b8fb0125cd752493a80
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-06-01 13:39:48 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 "use strict";
 
-// Show Menu
+// // // // // Show Menu Mobile // // // // //
 const navMenu = document.querySelector("#nav-menu");
 const navToggle = document.querySelector("#nav-toggle");
 const navClose = document.querySelector("#nav-close");
 
 // Show Menu
-navToggle && navToggle.addEventListener('click',() => {
-    navMenu.classList.add("show-menu")
-});
+navToggle &&
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 
 // Hide Menu
-navClose && navClose.addEventListener('click',() => {
-    navMenu.classList.remove("show-menu")
-});
+navClose &&
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+
+// // // // // Remove Menu Mobile // // // // //
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((e) => e.addEventListener("click", linkAction));
