5e46c2f7a45c6bafc4eb1e8b0ba721423e45ad91
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-06-01 13:22:24 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// Show Menu
+const navMenu = document.querySelector("#nav-menu");
+const navToggle = document.querySelector("#nav-toggle");
+const navClose = document.querySelector("#nav-close");
+
+// Show Menu
+navToggle && navToggle.addEventListener('click',() => {
+    navMenu.classList.add("show-menu")
+});
+
+// Hide Menu
+navClose && navClose.addEventListener('click',() => {
+    navMenu.classList.remove("show-menu")
+});
